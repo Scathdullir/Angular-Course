1c037eef99ab322cce6d89e777995678d4d19e6b
Jana Batskihh <jana2812@mail.ru>
2020-12-13 21:31:27 +0200

    angular components 2

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -12,6 +12,9 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -24,6 +27,9 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
     FlexLayoutModule
   ],
   providers: [],
