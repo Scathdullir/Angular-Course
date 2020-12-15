68ec4a1a9b0d8ac3b28d63bd58735f834864060e
Jana Batskihh <jana2812@mail.ru>
2020-12-15 22:32:12 +0200

    angular service basics

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -17,6 +17,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+import { from } from 'rxjs';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -34,7 +37,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatGridListModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
