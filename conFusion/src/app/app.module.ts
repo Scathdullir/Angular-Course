6b23fdb94d9d6260ec133605e401296471f816c3
Jana Batskihh <jana2812@mail.ru>
2020-12-20 23:15:10 +0200

    assignment 2

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -24,6 +24,8 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
+
 import { from } from 'rxjs';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
@@ -53,7 +55,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   ],
   providers: [
     DishService,
-    PromotionService
+    PromotionService,
+    LeaderService
   ],
   bootstrap: [AppComponent]
 })
